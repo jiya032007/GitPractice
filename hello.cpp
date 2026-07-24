911baacd23766cae79ee3c9df012171913e57d45
Jiya <jiya.rao.yadav@gmail.com>
2026-07-24 18:53:40 -0500

    first cpp program

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, what's your name? ";
+    string name;
+    cin >> name;
+    cout << "Nice to meet you, " << name << "!" << endl;
+    return 0;
+}

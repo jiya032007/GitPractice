b9665dd71e95f5fa64931330e73752e58754aa1c
Jiya <jiya.rao.yadav@gmail.com>
2026-07-24 18:58:09 -0500

    added age feature

--- hello.cpp
+++ hello.cpp
@@ -6,5 +6,9 @@ int main() {
     string name;
     cin >> name;
     cout << "Nice to meet you, " << name << "!" << endl;
+    cout << "How old are you? ";
+    int age;
+    cin >> age;
+    cout << "In 10 years, you'll be " << age + 10 << " years old." << endl;
     return 0;
 }
